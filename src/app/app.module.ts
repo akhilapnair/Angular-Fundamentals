e9524dd681e4946afd02c05a93b9b6668296f5c7
akhilavipin <akhi.prabha@gmail.com>
2018-08-16 15:31:49 +0530

    Nav bar and installed json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventComponent } from './event/event.component';
 import { EvetThumbnailComponent } from './event/evet-thumbnail/evet-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EventComponent,
-    EvetThumbnailComponent
+    EvetThumbnailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
